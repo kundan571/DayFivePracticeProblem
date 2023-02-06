da6d3f811af5cb77256167c8b831d9fd1e951e3c
kundan571 <kundanchauhan1997@gmail.com>
2023-02-07 01:14:59 +0530

    Added: Sum of three integer adds to zero

---
+++ src/functionalprogram/Sumoftripletiszero.java
@@ -0,0 +1,26 @@
+package functionalprogram;
+
+public class Sumoftripletiszero {
+    static void findTriplets(int[] array, int length) {
+        boolean found = false;
+        for (int i = 0; i < length - 2; i++) {
+            for (int j = i + 1; j < length - 1; j++) {
+                for (int k = j + 1; k < length; k++) {
+                    if (array[i] + array[j] + array[k] == 0) {
+                        System.out.println(array[i] + " " + array[j] + " " + array[k]);
+                        found = true;
+                    }
+                }
+            }
+        }
+        // If no triplet with 0 sum found in array
+        if (found == false)
+            System.out.println(" Triplet not exist ");
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {0, 2, -3, 1};
+        int length = arr.length;
+        findTriplets(arr, length);
+    }
+}
