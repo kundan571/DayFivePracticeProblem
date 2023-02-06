e9be3d50d65dbd7afd572ab9a012ad6fa0c9d0e2
kundan571 <kundanchauhan1997@gmail.com>
2023-02-07 01:02:35 +0530

    Added: Quadratic equation problem for finding roots

---
+++ src/functionalprogram/Quadraticequation.java
@@ -0,0 +1,32 @@
+package functionalprogram;
+
+import java.util.Scanner;
+
+public class Quadraticequation {
+
+    public static void main(String[] Strings) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the value of a: ");
+        double a = scanner.nextDouble();
+        System.out.println("Enter the value of b: ");
+        double b = scanner.nextDouble();
+        System.out.println("Enter the value of c: ");
+        double c = scanner.nextDouble();
+        double discriminant = b * b - 4.0 * a * c;
+        if (discriminant > 0.0) {
+            double r1 = (-b + Math.pow(discriminant, 0.5)) / (2.0 * a);
+            double r2 = (-b - Math.pow(discriminant, 0.5)) / (2.0 * a);
+            System.out.println("The roots are " + r1 + " and " + r2);
+        } else if (discriminant == 0.0) {
+            double r1 = -b / (2.0 * a);
+            System.out.println("The root is " + r1);
+        } else {
+            System.out.println("Roots are not real.");
+        }
+    }
+}
+
+
+
+
+
